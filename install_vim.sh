90ad1de362cfacf88db4dae3a2ec04edffae1acd
Satyajit Roy <email2sroy@gmail.com>
2016-01-19 15:56:23 -0800

    Removing the coloring from install script

--- install_vim.sh
+++ install_vim.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-ESCAPE="\033";
 
-echo "${ESCAPE}[32mWelcome to ViM dotfiles setup${ESCAPE}[0m\n"
-echo "${ESCAPE}[32mThis dotfile repo usages ${ESCAPE}[33mvim-plug${ESCAPE}[0m ${ESCAPE}[32mas plugin manager for vim, getting that from github${ESCAPE}[0m"
-echo "${ESCAPE}[32mDownloading the required vim settings and plugin list ${ESCAPE}[0m\n"
+echo "Welcome to ViM dotfiles setup\n"
+echo "This dotfile repo usages vim-plug as plugin manager for vim, getting that from github"
+echo "Downloading the required vim settings and plugin list\n"
 git clone https://github.com/git4sroy/vimdotfiles ${HOME}/.vimdotfiles
-echo "${ESCAPE}[32mLets create the symlink for ${ESCAPE}[32m.vimrc${ESCAPE}[0m"
+echo "Lets create the symlink for .vimrc"
 ln -s ${HOME}/.vimdotfiles/vimrc ${HOME}/.vimrc
 ln -s ${HOME}/.vimdotfiles/vim ${HOME}/.vim
-echo "${ESCAPE}[32mBoom plugins are on there way...${ESCAPE}[0m\n"
+echo "Boom plugins are on there way...\n"
 vim +PlugInstall +qall
