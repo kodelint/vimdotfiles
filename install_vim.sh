756fdd515c2a3fadc669515d5d6170dd68112927
Satyajit Roy <email2sroy@gmail.com>
2016-01-19 15:59:23 -0800

    Small decoration changes :)

--- install_vim.sh
+++ install_vim.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "Welcome to ViM dotfiles setup\n"
+echo "Welcome to ViM dotfiles setup"
 echo "This dotfile repo usages vim-plug as plugin manager for vim, getting that from github"
-echo "Downloading the required vim settings and plugin list\n"
+echo "Downloading the required vim settings and plugin list"
 git clone https://github.com/git4sroy/vimdotfiles ${HOME}/.vimdotfiles
 echo "Lets create the symlink for .vimrc"
 ln -s ${HOME}/.vimdotfiles/vimrc ${HOME}/.vimrc
 ln -s ${HOME}/.vimdotfiles/vim ${HOME}/.vim
-echo "Boom plugins are on there way...\n"
+echo "Boom plugins are on there way..."
 vim +PlugInstall +qall
