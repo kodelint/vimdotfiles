3f35a9f06af407db576713d04367a124f157b0eb
Satyajit Roy Choudhury <m535987@a4774217.federated.fds>
2018-03-01 23:54:58 -0800

    Remove ZSH Syntax plugin
    Vim Configuration is not the right place to have Zsh Plugins

--- install_vim.sh
+++ install_vim.sh
@@ -9,10 +9,4 @@ ln -s ${HOME}/.vimdotfiles/vimrc ${HOME}/.vimrc
 ln -s ${HOME}/.vimdotfiles/vim ${HOME}/.vim
 echo "Boom plugins are on there way..."
 vim +PlugInstall +qall
-echo "Let's install zsh-syntax-highlighting as well "
-cd /etc/yum.repos.d/
-sudo wget https://download.opensuse.org/repositories/shells:zsh-users:zsh-syntax-highlighting/CentOS_7/shells:zsh-users:zsh-syntax-highlighting.repo
-sudo yum install zsh-syntax-highlighting -y
-echo "install the Plugin"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
